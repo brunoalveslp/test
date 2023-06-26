7a2c33fce30b077ed2db11d02535d8e1024d3340
Bruno Alves <brunoalveslp200597@gmail.com>
2023-06-25 21:37:03 -0300

    versao 1.1 - algumas correcoes e melhorias

--- ATIVIDADE_02/js/main.js
+++ ATIVIDADE_02/js/main.js
@@ -1,3 +1,4 @@
+// Seletores
 
 const nome = document.querySelector('#nome');
 const cpf = document.querySelector('#cpf');
@@ -19,14 +20,20 @@ const inputs = document.querySelectorAll('.inputs');
 const messages = document.querySelectorAll('.message');
 const close_btns = document.querySelectorAll('.close-message');
 
-console.log(placa.value.length);
+// Event Listeners
 
 close_btns.forEach(element => {
-    element.addEventListener('click', (e) =>{
+    element.addEventListener('click', () =>{
         if(element.classList.contains('close-error')){
             messages[0].classList.remove('show');
         }else {
             messages[0].classList.remove('show');
+        }
+
+        if(element.classList.contains('close-success')){
+            messages[1].classList.remove('show');
+        }else {
+            messages[1].classList.remove('show');
         }
     });
 });
@@ -39,20 +46,20 @@ button.addEventListener("click", (event) => {
   }  
 });
 
-confirm_yes.addEventListener('click', (event)=>{
+confirm_yes.addEventListener('click', ()=>{
     validaFormulario();
     confirm_box.close();
 });
 
-confirm_no.addEventListener('click', (event)=>{
+confirm_no.addEventListener('click', ()=>{
     confirm_box.close();
 });
 
-close.addEventListener('click', (event)=>{
+close.addEventListener('click', ()=>{
     confirm_box.close();
 });
 
-cpf.addEventListener('keypress', (event) =>{
+cpf.addEventListener('keypress', () =>{
     if(cpf.value.length === 3 || cpf.value.length === 7){
         cpf.value += '.';
     } else if (cpf.value.length === 11){
@@ -62,77 +69,35 @@ cpf.addEventListener('keypress', (event) =>{
 
 cpf.addEventListener('blur', () => {
     if(Validador.Valida(cpf.value)){
-        cpf.classList.add('valido');
-        campos[2].classList.remove('error');
-        campos[2].classList.add('valido');
+        campos[2].classList.remove('error','invalido');
+        campos[1].classList.remove('error', 'invalido');
         cpf_span.classList.add('show');
     } else {
-        cpf.classList.remove('valido');
-        campos[2].classList.add('error');
-        campos[2].classList.remove('valido');
-        cpf_span.classList.add('show');
-        campos[2].classList.add('show');
+        campos[2].classList.remove('error', 'show');
+        campos[1].classList.add('show', 'invalido');
+        cpf_span.classList.remove('show')
     }
 });
 
 distancia.addEventListener('blur',() => {
-    if(distancia.value == 0){
-        distancia.classList.add('error');
-        campos[4].classList.add('show');
-    } else {
-        distancia.classList.remove('error');
-        campos[4].classList.remove('show');
-    }
+    validaValor(distancia,5);
 });
 
-valor_litro.addEventListener('blur', (event) => {
-    if(valor_litro.value == 0){
-        valor_litro.classList.add('error');
-        campos[5].classList.add('show');
-    } else {
-        valor_litro.classList.remove('error');
-        campos[5].classList.remove('show');
-    }
-
+valor_litro.addEventListener('blur', () => {
+    validaValor(valor_litro,6);
     valor_total.value = distancia.value * valor_litro.value;
 });
 
-valor_total.addEventListener('blur', (event) => {
-    if(valor_total.value == 0){
-        valor_total.classList.add('error');
-        campos[6].classList.add('show');
-    } else {
-        valor_total.classList.remove('error');
-        campos[6].classList.remove('show');
-    }
+valor_total.addEventListener('blur', () => {
+    validaValor(valor_total,7);
 });
 
 
-window.addEventListener('keypress', (event) => {
-    let index;
-    let len = 0;
-    let element = event.target.name;
-    if(element == 'nome'){
-        index = 0;
-        len = 4
-    } else if(element == 'numero_placa'){
-        index = 3;
-        len = 7;
-    } else if(element == 'data_nf'){
-        index = 3;
-        len = 1;
-    }
-
-    if(inputs[index]){
-        if(inputs[index].value.length < len){
-            inputs[index].classList.add('error');
-            campos[index].classList.add('show');
-        } else {
-            inputs[index].classList.remove('error');
-            campos[index].classList.remove('show');
-        }
-    }
+window.addEventListener('keypress', () => {
+    validaCamposDinamic();
 });
+
+// Functions
 
 const validaCampos = () => {
     let result = true;
@@ -180,23 +145,52 @@ const validaCampos = () => {
         result = false;
     }
 
+    if(result == false){
+        warning.classList.add('show');
+        removeClass();
+    }
+
     return result;
 };
 
-const validaFormulario = () => {
+const validaCamposDinamic = () => {
+    let index;
+    let len = 0;
+    let element = event.target.name;
+    if(element == 'nome'){
+        index = 0;
+        len = 4
+    } else if(element == 'numero_placa'){
+        index = 3;
+        len = 7;
+    } else if(element == 'data_nf'){
+        index = 3;
+        len = 1;
+    } else if(element == 'cpf'){
+        index = 1;
+        len = 13;
+    }
 
-    if(nome.value.length > 5 && cpf.value.length == 14  && placa.value.length == 7 && data.value != null && distancia.value > 0 && valor_litro.value > 0 && valor_total.value > 0 ){
-        success.classList.add('show');
-        removeClass();
-      } else {
-        if(nome.length < 3){
-
+    if(inputs[index]){
+        if(inputs[index].value.length < len){
+            inputs[index].classList.add('error');
+            campos[index].classList.add('show');
+        } else {
+            inputs[index].classList.remove('error');
+            campos[index].classList.remove('show');
         }
-        warning.classList.add('show');
-        removeClass();
-      }
-}
+    }
+};
 
+const validaValor = (element, index) => {
+    if(element.value == 0){
+        element.classList.add('error');
+        campos[index].classList.add('show');
+    } else {
+        element.classList.remove('error');
+        campos[index].classList.remove('show');
+    }
+}
 
 const removeClass = () => {
     setTimeout(() => {
